b17bb1a8993a44b1e00eef9658911ea0ab56e29b
joaovq <vitorsnta90@gmail.com>
2023-04-06 13:58:27 -0300

    update: solid principles explain

--- app/src/main/java/com/example/listapersonagens/network/authentication/Authenticator.kt
+++ app/src/main/java/com/example/listapersonagens/network/authentication/Authenticator.kt
@@ -1,5 +1,13 @@
 package com.example.listapersonagens.network.authentication
 
+// Depois
+// Criação de uma interface para a Autenticação
+/** Open for implement Closed for Modification =>
+ *   Utilização do conceito a partir da interface.
+ *   A utilização deste principio torna o código
+ *   mais fácil de manter porque basta passar a interface como dependência que
+ *   vamos prover a implementação do objeto que foi passado
+ * */
 interface Authenticator {
     fun login(email: String, senha: String): Boolean
 }
--- app/src/main/java/com/example/listapersonagens/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/listapersonagens/ui/fragment/LoginFragment.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.listapersonagens.R
 import com.example.listapersonagens.databinding.FragmentLoginBinding
+import com.example.listapersonagens.di.usecase.SignInUseCase
 import com.example.listapersonagens.network.authentication.FirebaseAuthenticator
 import com.example.listapersonagens.ui.utils.extension.toast
 
@@ -39,7 +39,17 @@ class LoginFragment : Fragment() {
     private fun setupView() {
         with(binding) {
             btnLogin.setOnClickListener {
-                val isLoggedIn = FirebaseAuthenticator.login(
+//                ANTES
+                /**
+                 *
+                 * val isLogged = FirebaseAuthenticator.login(
+                 tietEmail.text.toString(),
+                 tietPassword.text.toString(),
+                 )
+                 * */
+
+                val useCase: SignInUseCase = SignInUseCase(FirebaseAuthenticator)
+                val isLoggedIn = useCase(
                     tietEmail.text.toString(),
                     tietPassword.text.toString(),
                 )
